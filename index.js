724d261d47613fceaf8accf694d03a2c30d6a86c
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-31 14:35:23 -1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('./generated/prisma');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const post = await prisma.post.update({
+        where: { id: 1 },
+        data: { published: true },
+    });
+    console.log(post);
+};
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
